d755e146a410d266e029040d568d1941973d3221
Eder Henrique <ederhscc@gmail.com>
2023-10-03 15:59:58 -0300

    setting up react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import "./App.css";
+
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 
 function App() {
-
   return (
     <>
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
