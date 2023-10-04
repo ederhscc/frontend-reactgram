6f6b8349eed22c4602a69bdd7c4336eea5156fc4
Eder Henrique <ederhscc@gmail.com>
2023-10-04 11:26:11 -0300

    added navbar and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Components
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 // Pages
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Auth/Login.jsx";
@@ -12,11 +16,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register/>} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
