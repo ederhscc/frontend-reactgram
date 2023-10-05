cc3383559bfc861311b37b16d91172c8f78f431a
Eder Henrique <ederhscc@gmail.com>
2023-10-05 10:21:25 -0300

    added css of autentication form

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -7,23 +7,26 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Register = () => {
-
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
-  return <div>
-    <h2>ReactGram</h2>
-    <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Nome" />
-      <input type="text" placeholder="E-mail" />
-      <input type="password" placeholder="Senha" />
-      <input type="password" placeholder="Confirme a senha" />
-      <input type="submit" value="Cadastrar" />
-    </form>
-    <p>Já tem conta?<Link to="/login">Clique aqui</Link></p>
-  </div>;
+  return (
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta?<Link to="/login">Clique aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
