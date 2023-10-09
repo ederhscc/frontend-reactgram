518f471bc5adc407bce861a9cdefba7c6f023741
Eder Henrique <ederhscc@gmail.com>
2023-10-09 15:15:00 -0300

    added login form

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,7 +1,54 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message.jsx";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
