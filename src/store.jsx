f6795235ccf5729f3b55516878a33a26469083e5
Eder Henrique <ederhscc@gmail.com>
2023-10-11 10:42:25 -0300

    added user reducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlice.jsx";
+import userReducer from "./slices/userSlice.jsx";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
