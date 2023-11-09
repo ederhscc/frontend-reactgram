3318cf19292d53dcc760be3e6da24f0124ce23a2
Eder Henrique <ederhscc@gmail.com>
2023-11-09 20:28:24 -0300

    finalizing search

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -12,9 +12,49 @@ import PhotoItem from "../../components/PhotoItem.jsx";
 import { Link } from "react-router-dom";
 
 // redux
+import { searchPhotos, like } from "../../slices/photoSlice.jsx";
 
 const Search = () => {
-  return <div>Search</div>;
-};
+    const query = useQuery();
+    const search = query.get("q");
+  
+    const dispatch = useDispatch();
+  
+    const resetMessage = useResetComponentMessage(dispatch);
+  
+    const { user } = useSelector((state) => state.auth);
+    const { photos, loading } = useSelector((state) => state.photo);
+  
+    // Load all photos
+    useEffect(() => {
+      dispatch(searchPhotos(search));
+    }, [dispatch, search]);
+  
+    const handleLike = (photo = null) => {
+      dispatch(like(photo._id));
+  
+      resetMessage();
+    };
+  
+    if (loading) {
+      return <p>Carregando...</p>;
+    }
+  
+    return (
+      <div id="search">
+        <h2>Você está buscando por: {search}</h2>
+        {photos &&
+          photos.map((photo) => (
+            <div key={photo._id}>
+              <PhotoItem photo={photo} />
+              <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+              <Link className="btn" to={`/photos/${photo._id}`}>
+                Ver mais
+              </Link>
+            </div>
+          ))}
+      </div>
+    );
+  };
 
 export default Search;
